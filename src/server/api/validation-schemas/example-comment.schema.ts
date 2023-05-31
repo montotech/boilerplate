33012f02ae091f26f2eb258d203d6f14b22e8215
Liyu M <liyuumk@gmail.com>
2023-06-01 00:28:54 +0300

    Example comment schema and entity

---
+++ src/server/api/validation-schemas/example-comment.schema.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const validationSchemaForCreateExampleComment = z.object({
+  content: z.string().min(5),
+  postId: z.string(),
+});
+
+export type ValidationSchemaForCreateExampleComment = z.TypeOf<
+  typeof validationSchemaForCreateExampleComment
+>;
+
+export const validationSchemaForUpdateExampleComment =
+  validationSchemaForCreateExampleComment
+    .pick({
+      content: true,
+    })
+    .extend({
+      id: z.string(),
+    });
+
+export type ValidationSchemaForUpdateExampleComment = z.TypeOf<
+  typeof validationSchemaForUpdateExampleComment
+>;
+
+export const validationSchemaForListExampleComments =
+  validationSchemaForCreateExampleComment
+    .pick({
+      postId: true,
+    })
+    .extend({
+      limit: z.number().min(1).max(20).nullish(),
+      cursor: z.string().nullish(),
+    });
+
+export type ValidationSchemaForListExampleComments = z.TypeOf<
+  typeof validationSchemaForListExampleComments
+>;
